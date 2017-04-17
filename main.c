b638c33b281d9bb60915aad4bd7eeaf20f5dbf5a
Francois Perrad <francois.perrad@gadz.org>
2017-04-17 15:44:08 +0200

    don't care with the result of luaL_dostring

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int run_lua(const char* script) {
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 
-	int res = luaL_dostring(L, script);
+	(void)luaL_dostring(L, script);
 
 	size_t len = 0;
 	const char* value = lua_tolstring(L, lua_gettop(L), &len);
