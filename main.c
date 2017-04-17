d1a197150bf333f44227d0d91ff6f9979ec0dce3
Francois Perrad <francois.perrad@gadz.org>
2017-04-17 16:01:25 +0200

    start by variable declaration (C89)

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 #include <lualib.h>
 
 int run_lua(const char* script) {
+	size_t len;
+	const char* value;
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 
 	(void)luaL_dostring(L, script);
 
-	size_t len = 0;
-	const char* value = lua_tolstring(L, lua_gettop(L), &len);
+	value = lua_tolstring(L, lua_gettop(L), &len);
 	lua_writestring(value, len);
 	lua_writeline();
 
 	lua_close(L);
-
 	return 0;
 }
